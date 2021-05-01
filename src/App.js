d804602413b0dcd8a8351f9e25fdad63bce2a967
Basit Owaisi <basitowaisi@gmail.com>
2021-05-02 02:34:30 +0530

    Change demopage design

--- src/App.js
+++ src/App.js
@@ -3,16 +3,28 @@ import React from "react"
 
 function App() {
   return (
-    <div>
-      <Pikaday
-        id="dob"
-        name="dob"
-        onChange={(val) => console.log(val)}
-        value={new Date()}
-        placeholder="Please choose a date"
-        format="DD-MM-YYYY"
-      />
-    </div>
+    <>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "100vh",
+          flexDirection: "column",
+          background: "#fafafa",
+        }}
+      >
+        <h3>A Simple Wrapper for the existing Pikaday Date Picker</h3>
+        <Pikaday
+          id="dob"
+          name="dob"
+          onChange={(val) => console.log(val)}
+          value={new Date()}
+          placeholder="Please choose a date"
+          format="DD-MM-YYYY"
+        />
+      </div>
+    </>
   )
 }
 
